62e274919823092f60d0c4d8834a4acccde0e1bb
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2023-11-03 14:25:48 +0530

    first push

---
+++ GmailProject/src/test/java/gmail/SendMailTest.java
@@ -0,0 +1,11 @@
+package gmail;
+
+import org.testng.annotations.Test;
+
+public class SendMailTest {
+	@Test
+	public void demo() {
+		System.out.println("==========Send Mail executed========");
+	}
+
+}
